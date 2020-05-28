773c6cad51dbe2113fe6c7e53e8382902d6ff96d
esteban8at <esteban.ochoa@correounivalle.edu.co>
2020-05-27 21:27:55 -0500

    edificios

--- repro/conversor_shp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for f in *.shp
-do
-	archivo="${f%.*}"
-	echo "CONVIRTIENDO SHP"
-	shp2pgsql -g the_geom -W LATIN1 -s 4326 $archivo  $archivo>$archivo.sql
-done
-
-
--- repro/subir_shp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for f in *.sql
-do
-	archivo="${f%.*}"
-	echo "SUBIENDO"
-	psql -h localhost -p 5432 -d proy -U user -f $archivo.sql
-done 
